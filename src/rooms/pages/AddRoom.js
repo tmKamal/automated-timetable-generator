ef19957d47e58bf446750ddf8a8c8c02d57d95a4
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-08-19 02:34:23 +0530

    forms added

--- src/rooms/pages/AddRoom.js
+++ src/rooms/pages/AddRoom.js
@@ -1,7 +1,130 @@
-import React from 'react';
-const AddRoom =()=>{
-    return (
-        <div>AddRoom</div>
-    )
-}
+import React from "react";
+import { CssBaseline, Paper, Typography, Grid, TextField, FormControl, InputLabel, Select, MenuItem, Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginTop: theme.spacing(5),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AddRoom = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+
+      <main style={{ marginTop: "100px" }} className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography
+            style={{ marginBottom: "20px" }}
+            component="h1"
+            variant="h4"
+            align="center"
+          >
+            Add Room
+          </Typography>
+
+          <form onSubmit={submitHandler} className={classes.form} noValidate>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  onChange={onChangeHandler("roomName")}
+                  value={roomName}
+                  id="roomName"
+                  name="roomName"
+                  variant="outlined"
+                  label="RoomName"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl variant="outlined" className={classes.formControl}>
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    Age
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={age}
+                    onChange={handleChange}
+                    label="Age"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl variant="outlined" className={classes.formControl}>
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    Age
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={age}
+                    onChange={handleChange}
+                    label="Age"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              
+            </Grid>
+
+            <div className={classes.buttons}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Paper>
+      </main>
+    </React.Fragment>
+  );
+};
 export default AddRoom;
