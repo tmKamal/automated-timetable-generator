9b9516df1d1315463281b765e695abe9e7a0cf47
Ashen Kavinda <49136460+Ashenk97@users.noreply.github.com>
2020-10-08 19:51:52 +0530

    check those

--- src/tags/pages/AddTag.js
+++ src/tags/pages/AddTag.js
@@ -1,176 +1,162 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    CssBaseline,
-    Paper,
-    Typography,
-    Grid,
-    TextField,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    Button,
-    makeStyles
-} from '@material-ui/core';
-import { Alert, AlertTitle } from '@material-ui/lab';
+  CssBaseline,
+  Paper,
+  Typography,
+  Grid,
+  TextField,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import { Alert, AlertTitle } from "@material-ui/lab";
 
-import { useHttpClient } from '../../shared/custom-hooks/http-hook';
+import { useHttpClient } from "../../shared/custom-hooks/http-hook";
 
 const useStyles = makeStyles((theme) => ({
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing(2),
-        marginRight: theme.spacing(2),
-        marginTop: theme.spacing(5),
-        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-            width: 600,
-            marginLeft: 'auto',
-            marginRight: 'auto'
-        }
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginTop: theme.spacing(5),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto",
     },
-    paper: {
-        marginTop: theme.spacing(10),
-        marginBottom: theme.spacing(3),
-        padding: theme.spacing(2),
-        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-            marginTop: theme.spacing(6),
-            marginBottom: theme.spacing(6),
-            padding: theme.spacing(3)
-        }
+  },
+  paper: {
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
     },
+  },
 
-    buttons: {
-        display: 'flex',
-        justifyContent: 'flex-end'
-    },
-    button: {
-        marginTop: theme.spacing(3),
-        marginLeft: theme.spacing(1)
-    }
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
 }));
 
 const AddTag = () => {
-    const classes = useStyles();
-    const { isLoading, error, sendRequest, errorPopupCloser } = useHttpClient();
-    const [msg, setMsg] = useState();
-    const [values, setValues] = useState({
-        tagType: ''
-    });
+  const classes = useStyles();
+  const { error, sendRequest, errorPopupCloser } = useHttpClient();
+  const [msg, setMsg] = useState();
+  const [values, setValues] = useState({
+    tagType: "",
+  });
 
-    const { tagType } = values;
+  const { tagType } = values;
 
-    const onChangeHandler = (inputFieldName) => (e) => {
-        setValues({ ...values, [inputFieldName]: e.target.value });
-        setMsg(null);
-        errorPopupCloser();
+  const onChangeHandler = (inputFieldName) => (e) => {
+    setValues({ ...values, [inputFieldName]: e.target.value });
+    setMsg(null);
+    errorPopupCloser();
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    errorPopupCloser();
+    const location = {
+      tagType,
     };
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        errorPopupCloser();
-        const location = {
-            tagType
-        };
-        console.log(location);
-        try {
-            const responseData = await sendRequest(
-                'https://timetable-generator-api.herokuapp.com/api/tag/',
-                'POST',
-                JSON.stringify(location),
-                { 'Content-Type': 'application/json' }
-            );
-            if (error) {
-                console.log(error);
-            }
-            console.log(responseData);
-            if (responseData) {
-                setValues({
-                    tagType: ''
-                });
-                console.log(responseData);
-                setMsg(responseData.msg);
-            }
-        } catch (err) {
-            console.log(error);
-        }
-    };
+    console.log(location);
+    try {
+      const responseData = await sendRequest(
+        "https://timetable-generator-api.herokuapp.com/api/tag/",
+        "POST",
+        JSON.stringify(location),
+        { "Content-Type": "application/json" }
+      );
+      if (error) {
+        console.log(error);
+      }
+      console.log(responseData);
+      if (responseData) {
+        setValues({
+          tagType: "",
+        });
+        console.log(responseData);
+        setMsg(responseData.msg);
+      }
+    } catch (err) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <React.Fragment>
-            <CssBaseline />
+  return (
+    <React.Fragment>
+      <CssBaseline />
 
-            <main style={{ marginTop: '100px' }} className={classes.layout}>
-                <Paper className={classes.paper}>
-                    <Typography
-                        style={{ marginBottom: '20px' }}
-                        component='h1'
-                        variant='h4'
-                        align='center'
-                    >
-                        Add Tag
-                    </Typography>
+      <main style={{ marginTop: "100px" }} className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography
+            style={{ marginBottom: "20px" }}
+            component="h1"
+            variant="h4"
+            align="center"
+          >
+            Add Tag
+          </Typography>
 
-                    <form
-                        onSubmit={submitHandler}
-                        className={classes.form}
-                        noValidate
-                    >
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    onChange={onChangeHandler('tagType')}
-                                    value={tagType}
-                                    id='tagType'
-                                    name='tagType'
-                                    variant='outlined'
-                                    label='Tag Type'
-                                    error={
-                                        error.param === 'tagType' ? true : false
-                                    }
-                                    helperText={
-                                        error.param === 'tagType'
-                                            ? error.msg
-                                            : ''
-                                    }
-                                    fullWidth
-                                />
-                            </Grid>
-                            {error && (
-                                <Grid item xs={12}>
-                                    <Alert severity='error'>
-                                        <AlertTitle>Error</AlertTitle>
-                                        <strong>
-                                            {error.backendMsg
-                                                ? error.backendMsg
-                                                : 'Please Resolve the above error & try again'}{' '}
-                                        </strong>
-                                    </Alert>
-                                </Grid>
-                            )}
-                            {msg && (
-                                <Grid item xs={12}>
-                                    <Alert severity='success'>
-                                        <AlertTitle>Success !!</AlertTitle>
-                                        {msg}
-                                    </Alert>
-                                </Grid>
-                            )}
-                        </Grid>
+          <form onSubmit={submitHandler} className={classes.form} noValidate>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  onChange={onChangeHandler("tagType")}
+                  value={tagType}
+                  id="tagType"
+                  name="tagType"
+                  variant="outlined"
+                  label="Tag Type"
+                  error={error.param === "tagType" ? true : false}
+                  helperText={error.param === "tagType" ? error.msg : ""}
+                  fullWidth
+                />
+              </Grid>
+              {error && (
+                <Grid item xs={12}>
+                  <Alert severity="error">
+                    <AlertTitle>Error</AlertTitle>
+                    <strong>
+                      {error.backendMsg
+                        ? error.backendMsg
+                        : "Please Resolve the above error & try again"}{" "}
+                    </strong>
+                  </Alert>
+                </Grid>
+              )}
+              {msg && (
+                <Grid item xs={12}>
+                  <Alert severity="success">
+                    <AlertTitle>Success !!</AlertTitle>
+                    {msg}
+                  </Alert>
+                </Grid>
+              )}
+            </Grid>
 
-                        <div className={classes.buttons}>
-                            <Button
-                                type='submit'
-                                variant='contained'
-                                color='primary'
-                                className={classes.button}
-                            >
-                                Submit
-                            </Button>
-                        </div>
-                    </form>
-                </Paper>
-            </main>
-        </React.Fragment>
-    );
+            <div className={classes.buttons}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Paper>
+      </main>
+    </React.Fragment>
+  );
 };
 export default AddTag;
--- src/tags/pages/ViewTag.js
+++ src/tags/pages/ViewTag.js
@@ -1,229 +1,218 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    Grid,
-    TableContainer,
-    Table,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableBody,
-    IconButton,
-    Typography,
-    Button,
-    makeStyles,
-    Paper,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogContentText,
-    DialogActions
-} from '@material-ui/core';
-import { Alert } from '@material-ui/lab';
-import CreateIcon from '@material-ui/icons/Create';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { green, red } from '@material-ui/core/colors';
-import { Link } from 'react-router-dom';
-import { useHttpClient } from '../../shared/custom-hooks/http-hook';
+  Grid,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  IconButton,
+  Typography,
+  Button,
+  makeStyles,
+  Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import CreateIcon from "@material-ui/icons/Create";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { green, red } from "@material-ui/core/colors";
+import { Link } from "react-router-dom";
+import { useHttpClient } from "../../shared/custom-hooks/http-hook";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex'
-    },
+  root: {
+    display: "flex",
+  },
 
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4)
-    },
-    table: {
-        minWidth: 650
-    },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  table: {
+    minWidth: 650,
+  },
 
-    marginT: {
-        marginTop: '2rem'
-    }
+  marginT: {
+    marginTop: "2rem",
+  },
 }));
 
 const ViewTags = () => {
-    const classes = useStyles();
-    const [loadedTags, setLoadedTags] = useState();
-    const { isLoading, error, sendRequest, errorPopupCloser } = useHttpClient();
-    const [deleteId, setDeleteId] = useState();
-    const [openDialog, setOpenDialog] = useState(false);
-    const [deleteSuccMsg, setDeleteSuccMsg] = useState();
-    const [reload, setReload] = useState(); //use to fetch tags again, after deleting a tag.
+  const classes = useStyles();
+  const [loadedTags, setLoadedTags] = useState();
+  const { isLoading, sendRequest } = useHttpClient();
+  const [deleteId, setDeleteId] = useState();
+  const [openDialog, setOpenDialog] = useState(false);
+  const [deleteSuccMsg, setDeleteSuccMsg] = useState();
+  const [reload, setReload] = useState(); //use to fetch tags again, after deleting a tag.
 
-    useEffect(() => {
-        const fetchingTags = async () => {
-            try {
-                setLoadedTags(
-                    await sendRequest(
-                        'https://timetable-generator-api.herokuapp.com/api/tag'
-                    )
-                );
-            } catch (err) {}
-        };
-        fetchingTags();
-    }, [sendRequest, reload]);
-
-    const deleteDialogOpener = (id) => {
-        setDeleteId(id);
-        setOpenDialog(true);
+  useEffect(() => {
+    const fetchingTags = async () => {
+      try {
+        setLoadedTags(
+          await sendRequest(
+            "https://timetable-generator-api.herokuapp.com/api/tag"
+          )
+        );
+      } catch (err) {}
     };
-    const deleteDialogCloser = () => {
-        setOpenDialog(false);
-        setDeleteId(null);
-    };
-    const tagDeleteHandler = () => {
-        const deleteTag = async () => {
-            try {
-                const delMsg = await sendRequest(
-                    `https://timetable-generator-api.herokuapp.com/api/tag/${deleteId}`,
-                    'DELETE',
-                    null,
-                    {}
-                );
-                if (delMsg) {
-                    setDeleteId(null);
-                    deleteDialogCloser();
-                    setDeleteSuccMsg(true);
-                    setReload(!reload);
-                }
-            } catch (error) {
-                deleteDialogCloser();
-            }
-        };
-        deleteTag();
-    };
+    fetchingTags();
+  }, [sendRequest, reload]);
 
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <Typography
-                    variant='h5'
-                    align='center'
-                    color='textSecondary'
-                    paragraph
-                    className={classes.marginT}
-                >
-                    Tags
-                </Typography>
-            </Grid>
-            <Grid item xs={12}>
-                {deleteSuccMsg && (
-                    <Alert
-                        onClose={() => {
-                            setDeleteSuccMsg(false);
-                        }}
-                    >
-                        Tag has been successfully deleted.
-                    </Alert>
-                )}
+  const deleteDialogOpener = (id) => {
+    setDeleteId(id);
+    setOpenDialog(true);
+  };
+  const deleteDialogCloser = () => {
+    setOpenDialog(false);
+    setDeleteId(null);
+  };
+  const tagDeleteHandler = () => {
+    const deleteTag = async () => {
+      try {
+        const delMsg = await sendRequest(
+          `https://timetable-generator-api.herokuapp.com/api/tag/${deleteId}`,
+          "DELETE",
+          null,
+          {}
+        );
+        if (delMsg) {
+          setDeleteId(null);
+          deleteDialogCloser();
+          setDeleteSuccMsg(true);
+          setReload(!reload);
+        }
+      } catch (error) {
+        deleteDialogCloser();
+      }
+    };
+    deleteTag();
+  };
 
-                <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label='simple table'>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align='left'>Tag types</TableCell>
-                                <TableCell align='center'>Edit</TableCell>
-                                <TableCell align='center'>Delete</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {!isLoading &&
-                                loadedTags &&
-                                loadedTags.tags.map((tag) => (
-                                    <TableRow key={tag.id}>
-                                        <TableCell component='th' scope='row'>
-                                            {tag.tagType}
-                                        </TableCell>
-                                        <TableCell align='center'>
-                                            <Link to={`/update/tag/${tag.id}`}>
-                                                <IconButton
-                                                    color='secondary'
-                                                    aria-label='Edit the tag'
-                                                >
-                                                    <CreateIcon
-                                                        style={{
-                                                            color: green[500]
-                                                        }}
-                                                    />
-                                                </IconButton>
-                                            </Link>
-                                        </TableCell>
-                                        <TableCell align='center'>
-                                            <IconButton
-                                                onClick={() =>
-                                                    deleteDialogOpener(tag.id)
-                                                }
-                                                color='secondary'
-                                                aria-label='add an alarm'
-                                            >
-                                                <DeleteIcon
-                                                    style={{ color: red[500] }}
-                                                />
-                                            </IconButton>
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                {!isLoading && loadedTags && loadedTags.tags.length === 0 && (
-                    <React.Fragment>
-                        <Typography
-                            variant='h5'
-                            align='center'
-                            color='textSecondary'
-                            paragraph
-                            className={classes.marginT}
-                        >
-                            You haven’t Created Any Tags yet
-                        </Typography>
-                        <div>
-                            <Grid container spacing={2} justify='center'>
-                                <Grid item>
-                                    <Button
-                                        href='/admin/crud/tag-create'
-                                        variant='outlined'
-                                        color='primary'
-                                    >
-                                        Create One Now
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </div>
-                    </React.Fragment>
-                )}
-            </Grid>
-            {/* PopUp Dialog for delete confirmation*/}
-            <Dialog
-                open={openDialog}
-                onClose={deleteDialogCloser}
-                aria-labelledby='alert-dialog-title'
-                aria-describedby='alert-dialog-description'
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography
+          variant="h5"
+          align="center"
+          color="textSecondary"
+          paragraph
+          className={classes.marginT}
+        >
+          Tags
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        {deleteSuccMsg && (
+          <Alert
+            onClose={() => {
+              setDeleteSuccMsg(false);
+            }}
+          >
+            Tag has been successfully deleted.
+          </Alert>
+        )}
+
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Tag types</TableCell>
+                <TableCell align="center">Edit</TableCell>
+                <TableCell align="center">Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!isLoading &&
+                loadedTags &&
+                loadedTags.tags.map((tag) => (
+                  <TableRow key={tag.id}>
+                    <TableCell component="th" scope="row">
+                      {tag.tagType}
+                    </TableCell>
+                    <TableCell align="center">
+                      <Link to={`/update/tag/${tag.id}`}>
+                        <IconButton color="secondary" aria-label="Edit the tag">
+                          <CreateIcon
+                            style={{
+                              color: green[500],
+                            }}
+                          />
+                        </IconButton>
+                      </Link>
+                    </TableCell>
+                    <TableCell align="center">
+                      <IconButton
+                        onClick={() => deleteDialogOpener(tag.id)}
+                        color="secondary"
+                        aria-label="add an alarm"
+                      >
+                        <DeleteIcon style={{ color: red[500] }} />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {!isLoading && loadedTags && loadedTags.tags.length === 0 && (
+          <React.Fragment>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+              className={classes.marginT}
             >
-                <DialogTitle id='alert-dialog-title'>
-                    {'Delete confiramtion dialog box?'}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id='alert-dialog-description'>
-                        Are you really wanted to delete this Tag?
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={tagDeleteHandler} color='primary'>
-                        Yes
-                    </Button>
-                    <Button
-                        onClick={deleteDialogCloser}
-                        color='primary'
-                        autoFocus
-                    >
-                        No
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </Grid>
-    );
+              You haven’t Created Any Tags yet
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button
+                    href="/admin/crud/tag-create"
+                    variant="outlined"
+                    color="primary"
+                  >
+                    Create One Now
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </React.Fragment>
+        )}
+      </Grid>
+      {/* PopUp Dialog for delete confirmation*/}
+      <Dialog
+        open={openDialog}
+        onClose={deleteDialogCloser}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete confiramtion dialog box?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you really wanted to delete this Tag?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={tagDeleteHandler} color="primary">
+            Yes
+          </Button>
+          <Button onClick={deleteDialogCloser} color="primary" autoFocus>
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Grid>
+  );
 };
 export default ViewTags;
